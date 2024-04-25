d1cd544411a9e95554ae580eac13f2383799998f
Thomas2809 <166612110+Thomas2809@users.noreply.github.com>
2024-04-25 16:53:27 +0200

    reception_menu
    test menu

--- reception_moteur_2024.ino
+++ reception_moteur_2024.ino
@@ -7,9 +7,9 @@
  */
 
 // Inclusion des bibliothèques nécessaires
-#include <ESP32Servo.h>     // Bibliothèque pour contrôler les servomoteurs avec l'ESP32
-#include <Wire.h>           // Bibliothèque pour la communication I2C
-#include "BluetoothSerial.h"// Bibliothèque pour la communication Bluetooth
+#include <ESP32Servo.h>       // Bibliothèque pour contrôler les servomoteurs avec l'ESP32
+#include <Wire.h>             // Bibliothèque pour la communication I2C
+#include "BluetoothSerial.h"  // Bibliothèque pour la communication Bluetooth
 #include "m5rotate8.h"
 
 // Vérifie si le Bluetooth est activé
@@ -43,17 +43,17 @@ char pos4 = 0;
 M5ROTATE8 MM;
 
 // Informations sur le périphérique ESP32 Bluetooth (Esclave)
-const char *pin = "1234";  // Code PIN pour le couplage Bluetooth
-String device_name = "ESP32-BT-Slave"; // Nom du périphérique Bluetooth
+const char *pin = "1234";               // Code PIN pour le couplage Bluetooth
+String device_name = "ESP32-BT-Slave";  // Nom du périphérique Bluetooth
 int servoPouce = 13;
 int servoINDEX = 12;
 int servoMAJEUR = 27;
 int servoAnnulaire = 33;
-int servoAuriculaire = 15; // Broche pour le servomoteur Auriculaire
+int servoAuriculaire = 15;  // Broche pour le servomoteur Auriculaire
 
-char receiveData[7];        // Tableau pour stocker les données reçues
-#define NB_data 7           // Nombre d'éléments dans le tableau receiveData
-
+char receiveData[7];  // Tableau pour stocker les données reçues
+#define NB_data 7     // Nombre d'éléments dans le tableau receiveData
+char mode ;
 // Fonction de configuration, appelée une fois au démarrage
 void setup() {
   // Attacher les servomoteurs à leurs broches respectives
@@ -65,8 +65,7 @@ void setup() {
 
   // Initialisation de la communication I2C
   Wire.begin();
-
-  // Initialisation de la communication série avec un débit de 115200 bauds
+  
   Serial.begin(115200);
 
   // Initialisation de la communication série Bluetooth
@@ -87,7 +86,62 @@ void setup() {
 // Fonction de boucle principale
 void loop() {
   // Vérifie si des données sont disponibles sur le port série et les envoie via Bluetooth
-  if (Serial.available()) {
+  if (SerialBT.available()) 
+  {
+    SerialBT.write(Serial.read());
+     mode = SerialBT.read();  // Lire le caractère envoyé via Bluetooth
+  }
+    // Interpréter le caractère reçu pour déterminer le mode sélectionné
+    switch (mode) {
+      case '$':
+        Serial.println("mode_bt");
+        // Lit le premier caractère des données reçues
+        receiveData[0] = SerialBT.read();
+        // Vérifie si le premier caractère est '#'
+        if (SerialBT.available()) {
+
+          if (receiveData[0] == '#') {
+            // Parcourt les données restants
+            for (char cptSend = 1; cptSend < NB_data; cptSend++) {
+              // Attend que des données soient disponibles
+              while (!SerialBT.available());
+              // Lit et stocke les données
+              receiveData[cptSend] = SerialBT.read();
+            }
+          }
+
+          // Affiche les données reçues
+          Serial.println(receiveData[0]);
+          Serial.printf("a:%d\n", receiveData[1]);
+          Serial.printf("b:%d\n", receiveData[2]);
+          Serial.printf("c:%d\n", receiveData[3]);
+          Serial.printf("d:%d\n", receiveData[4]);
+          Serial.printf("e:%d\n", receiveData[5]);
+          Serial.printf("f:%d\n", receiveData[6]);
+
+          // Définit les positions des servomoteurs en fonction des données reçues
+          myservoPouce.write(receiveData[1]);
+          myservoINDEX.write(receiveData[2]);
+          myservoMajeur.write(receiveData[3]);
+          myservoAnnulaire.write(receiveData[4]);
+          myservoAuriculaire.write(receiveData[5]);
+
+          // Vérifie si les données reçues sont valides
+          if (receiveData[0] == '#' && receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
+            Serial.print("Données OK\n");
+          else
+            Serial.print("Données INVALIDES\n");
+
+          // Efface le tampon de réception
+          receiveData[0] = 0;
+          break;
+          // Ajoutez d'autres cas pour d'autres modes si nécessaire
+        }
+    }
+  }
+
+
+  /*if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
 
@@ -132,4 +186,5 @@ void loop() {
     // Efface le tampon de réception
     receiveData[0] = 0;
   }
-}
+}*/
+
