15315c54b346d5c5e2bd2ef41e7035e256242204
Thomas2809 <166612110+Thomas2809@users.noreply.github.com>
2024-05-22 21:52:03 +0200

    reception du menu

--- reception_moteur_2024.ino
+++ reception_moteur_2024.ino
@@ -1,7 +1,7 @@
 /*
  * Nom du LAB : Communication Bluetooth ESP32->ESP32 (CODE ESCLAVE)
  *   [ CODE ESCLAVE ]
- * Auteur : Khaled Magdy
+ * Auteur : Thomas Giarrizzo
  * DeepBlueMbedded 2023
  * Pour plus d'informations, visitez : www.DeepBlueMbedded.com
  */
@@ -32,12 +32,23 @@ Servo myservoMajeur;
 Servo myservoAnnulaire;
 Servo myservoAuriculaire;
 
+Servo myservo;
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+
+#define NB_data 7  // Nombre d'éléments dans le tableau receiveData
+#define MODE_MENU 1
+#define MODE_MANUEL 2
+#define MODE_BT 3
 // Variables pour stocker les positions des servomoteurs
 char pos = 0;
 char pos1 = 0;
 char pos2 = 0;
 char pos3 = 0;
 char pos4 = 0;
+char receiveData[7];  // Tableau pour stocker les données reçues
 
 // Déclaration de l'objet M5ROTATE8
 M5ROTATE8 MM;
@@ -50,10 +61,23 @@ int servoINDEX = 12;
 int servoMAJEUR = 27;
 int servoAnnulaire = 33;
 int servoAuriculaire = 15;  // Broche pour le servomoteur Auriculaire
+bool inOptionsMenu = false;
 
-char receiveData[7];  // Tableau pour stocker les données reçues
-#define NB_data 7     // Nombre d'éléments dans le tableau receiveData
-char mode ;
+int mappedValue;
+int encoderValue;
+int mappedValue1;
+int encoderValue1;
+int mappedValue2;
+int encoderValue2;
+int mappedValue3;
+int encoderValue3;
+int mappedValue4;
+int encoderValue4;
+char caracter_recu;
+
+uint8_t R = 0, G = 0, B = 0;  //  colors
+uint16_t D = 0;
+// Fonction de configuration, appelée une fois au démarrage
 // Fonction de configuration, appelée une fois au démarrage
 void setup() {
   // Attacher les servomoteurs à leurs broches respectives
@@ -65,126 +89,239 @@ void setup() {
 
   // Initialisation de la communication I2C
   Wire.begin();
-  
   Serial.begin(115200);
-
   // Initialisation de la communication série Bluetooth
   SerialBT.begin(device_name);
-
   // Affichage des informations sur le périphérique
   Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-
   // Définition du code PIN pour le couplage Bluetooth
   SerialBT.setPin(pin);
   Serial.println("Using PIN");
-
   // Initialisation de l'objet M5ROTATE8
   MM.begin();
+  Serial.print("M5ROTATE8_LIB_VERSION: ");
+  Serial.println(M5ROTATE8_LIB_VERSION);
   MM.resetAll();
+
 }
 
 // Fonction de boucle principale
 void loop() {
   // Vérifie si des données sont disponibles sur le port série et les envoie via Bluetooth
-  if (SerialBT.available()) 
-  {
-    SerialBT.write(Serial.read());
-     mode = SerialBT.read();  // Lire le caractère envoyé via Bluetooth
-  }
-    // Interpréter le caractère reçu pour déterminer le mode sélectionné
-    switch (mode) {
-      case '$':
-        Serial.println("mode_bt");
-        // Lit le premier caractère des données reçues
-        receiveData[0] = SerialBT.read();
-        // Vérifie si le premier caractère est '#'
-        if (SerialBT.available()) {
-
-          if (receiveData[0] == '#') {
-            // Parcourt les données restants
-            for (char cptSend = 1; cptSend < NB_data; cptSend++) {
-              // Attend que des données soient disponibles
-              while (!SerialBT.available());
-              // Lit et stocke les données
-              receiveData[cptSend] = SerialBT.read();
-            }
-          }
-
-          // Affiche les données reçues
-          Serial.println(receiveData[0]);
-          Serial.printf("a:%d\n", receiveData[1]);
-          Serial.printf("b:%d\n", receiveData[2]);
-          Serial.printf("c:%d\n", receiveData[3]);
-          Serial.printf("d:%d\n", receiveData[4]);
-          Serial.printf("e:%d\n", receiveData[5]);
-          Serial.printf("f:%d\n", receiveData[6]);
-
-          // Définit les positions des servomoteurs en fonction des données reçues
-          myservoPouce.write(receiveData[1]);
-          myservoINDEX.write(receiveData[2]);
-          myservoMajeur.write(receiveData[3]);
-          myservoAnnulaire.write(receiveData[4]);
-          myservoAuriculaire.write(receiveData[5]);
-
-          // Vérifie si les données reçues sont valides
-          if (receiveData[0] == '#' && receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
-            Serial.print("Données OK\n");
-          else
-            Serial.print("Données INVALIDES\n");
-
-          // Efface le tampon de réception
-          receiveData[0] = 0;
-          break;
-          // Ajoutez d'autres cas pour d'autres modes si nécessaire
-        }
-    }
-  }
-
-
-  /*if (Serial.available()) {
+  if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
 
-  // Vérifie si des données sont disponibles sur le port série Bluetooth
+  // Vérifie si des données sont disponibles sur le Bluetooth
   if (SerialBT.available()) {
-    // Lit le premier caractère des données reçues
-    receiveData[0] = SerialBT.read();
+    char caracter_recu = SerialBT.read();  // Lire le caractère envoyé via Bluetooth
+    receiveData[0] = caracter_recu;
 
-    // Vérifie si le premier caractère est '#'
-    if (receiveData[0] == '#') {
-      // Parcourt les données restants
+    // Interpréter le caractère reçu pour déterminer le mode sélectionné
+    if (receiveData[0] == '&') {
+      // Parcourt les données restantes
       for (char cptSend = 1; cptSend < NB_data; cptSend++) {
         // Attend que des données soient disponibles
         while (!SerialBT.available());
         // Lit et stocke les données
         receiveData[cptSend] = SerialBT.read();
       }
+
+      // Affiche les données reçues
+      Serial.println(receiveData[0]);
+      Serial.printf("a:%d\n", receiveData[1]);
+      Serial.printf("b:%d\n", receiveData[2]);
+      Serial.printf("c:%d\n", receiveData[3]);
+      Serial.printf("d:%d\n", receiveData[4]);
+      Serial.printf("e:%d\n", receiveData[5]);
+      Serial.printf("f:%d\n", receiveData[6]);
+
+      Serial.println("CHOIX MENU");
+
+      // Vérifie si les données reçues sont valides
+      if (receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
+        Serial.print("Données OK\n");
+      else
+        Serial.print("Données INVALIDES\n");
+
+      // Efface le tampon de réception
+      receiveData[0] = 0;
     }
 
-    // Affiche les données reçues
-    Serial.println(receiveData[0]);
-    Serial.printf("a:%d\n", receiveData[1]);
-    Serial.printf("b:%d\n", receiveData[2]);
-    Serial.printf("c:%d\n", receiveData[3]);
-    Serial.printf("d:%d\n", receiveData[4]);
-    Serial.printf("e:%d\n", receiveData[5]);
-    Serial.printf("f:%d\n", receiveData[6]);
+    if (receiveData[0] == '@') {
+      // Parcourt les données restantes
+      for (char cptSend = 1; cptSend < NB_data; cptSend++) {
+        // Attend que des données soient disponibles
+        while (!SerialBT.available());
+        // Lit et stocke les données
+        receiveData[cptSend] = SerialBT.read();
+      }
 
-    // Définit les positions des servomoteurs en fonction des données reçues
-    myservoPouce.write(receiveData[1]);
-    myservoINDEX.write(receiveData[2]);
-    myservoMajeur.write(receiveData[3]);
-    myservoAnnulaire.write(receiveData[4]);
-    myservoAuriculaire.write(receiveData[5]);
+      // Affiche les données reçues
+      Serial.println(receiveData[0]);
+      Serial.printf("a:%d\n", receiveData[1]);
+      Serial.printf("b:%d\n", receiveData[2]);
+      Serial.printf("c:%d\n", receiveData[3]);
+      Serial.printf("d:%d\n", receiveData[4]);
+      Serial.printf("e:%d\n", receiveData[5]);
+      Serial.printf("f:%d\n", receiveData[6]);
 
-    // Vérifie si les données reçues sont valides
-    if (receiveData[0] == '#' && receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
-      Serial.print("Données OK\n");
-    else
-      Serial.print("Données INVALIDES\n");
+      // Définit les positions des servomoteurs en fonction des encodeurs
+      Serial.println("MODE MANUEL");
+      pouce();
+      index();
+      majeur();
+      annulaire();
+      auriculaire();
 
-    // Efface le tampon de réception
-    receiveData[0] = 0;
+      // Vérifie si les données reçues sont valides
+      if (receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
+        Serial.print("Données OK\n");
+      else
+        Serial.print("Données INVALIDES\n");
+
+      // Efface le tampon de réception
+      receiveData[0] = 0;
+    }
+
+    if (receiveData[0] == '#') {
+      // Parcourt les données restantes
+      for (char cptSend = 1; cptSend < NB_data; cptSend++) {
+        // Attend que des données soient disponibles
+        while (!SerialBT.available());
+        // Lit et stocke les données
+        receiveData[cptSend] = SerialBT.read();
+      }
+
+      // Affiche les données reçues
+      Serial.println(receiveData[0]);
+      Serial.printf("a:%d\n", receiveData[1]);
+      Serial.printf("b:%d\n", receiveData[2]);
+      Serial.printf("c:%d\n", receiveData[3]);
+      Serial.printf("d:%d\n", receiveData[4]);
+      Serial.printf("e:%d\n", receiveData[5]);
+      Serial.printf("f:%d\n", receiveData[6]);
+
+      // Définit les positions des servomoteurs en fonction des données reçues
+      Serial.println("mode bt");
+      myservoPouce.write(receiveData[1]);
+      myservoINDEX.write(receiveData[2]);
+      myservoMajeur.write(receiveData[3]);
+      myservoAnnulaire.write(receiveData[4]);
+      myservoAuriculaire.write(receiveData[5]);
+
+      // Vérifie si les données reçues sont valides
+      if (receiveData[6] == (receiveData[1] ^ receiveData[2] ^ receiveData[3] ^ receiveData[4] ^ receiveData[5]))
+        Serial.print("Données OK\n");
+      else
+        Serial.print("Données INVALIDES\n");
+
+      // Efface le tampon de réception
+      receiveData[0] = 0;
+    }
   }
-}*/
+}
 
+
+
+
+void pouce() {
+  encoderValue = MM.getAbsCounter(0);  // Lecture de la position de l'encodeur
+  mappedValue = map(255 - encoderValue, 0, 100, 50, 100);
+
+  // Vérification des limites de l'encodeur
+  if (encoderValue <= 0) {
+    encoderValue = 0;
+    MM.setAbsCounter(0, 0);  // Réinitialisez l'encodeur à 0
+
+  } else if (encoderValue >= 350) {
+    encoderValue = 350;        // Forcer encoderValue à 350 si c'est le cas
+    MM.setAbsCounter(0, 350);  // Réinitialisez l'encodeur à 0
+  }
+  // Positionnement du servomoteur en fonction de la valeur mappée
+  myservoPouce.write(mappedValue);
+  Serial.print(encoderValue);
+  delay(10);  // Délai entre chaque itération
+}
+
+void index() {
+  encoderValue1 = MM.getAbsCounter(1);  // Lecture de la position de l'encodeur
+  mappedValue1 = map(encoderValue1, 0, 100, 50, 100);
+
+  // Vérification des limites de l'encodeur
+  if (encoderValue1 <= 0) {
+    encoderValue1 = 0;
+    MM.setAbsCounter(1, 0);  // Réinitialisez l'encodeur à 0
+
+  } else if (encoderValue1 >= 220) {
+    encoderValue1 = 220;       // Forcer encoderValue à 350 si c'est le cas
+    MM.setAbsCounter(1, 220);  // Réinitialisez l'encodeur à 0
+  }
+  // Positionnement du servomoteur en fonction de la valeur mappée
+  myservoINDEX.write(mappedValue1);
+  Serial.print(encoderValue1);
+  delay(10);  // Délai entre chaque itération
+}
+
+void majeur() {
+  encoderValue2 = MM.getAbsCounter(2);  // Lecture de la position de l'encodeur
+  mappedValue2 = map(encoderValue2, 0, 100, 50, 100);
+
+  // Vérification des limites de l'encodeur
+  if (encoderValue2 <= 0) {
+    encoderValue2 = 0;
+    MM.setAbsCounter(2, 0);  // Réinitialisez l'encodeur à 0
+
+
+  } else if (encoderValue2 >= 220) {
+    encoderValue2 = 220;       // Forcer encoderValue à 350 si c'est le cas
+    MM.setAbsCounter(2, 220);  // Réinitialisez l'encodeur à 0
+  }
+  // Positionnement du servomoteur en fonction de la valeur mappée
+  myservoMajeur.write(mappedValue2);
+  // Affichage de la valeur de l'encodeur
+  Serial.print(encoderValue2);
+  delay(10);  // Délai entre chaque itération
+}
+void annulaire() {
+  encoderValue3 = MM.getAbsCounter(3);  // Lecture de la position de l'encodeur
+  mappedValue3 = map(encoderValue3, 0, 100, 50, 100);
+
+  // Vérification des limites de l'encodeur
+  if (encoderValue3 <= 0) {
+    encoderValue3 = 0;
+    MM.setAbsCounter(3, 0);  // Réinitialisez l'encodeur à 0
+
+  } else if (encoderValue3 >= 220) {
+    encoderValue3 = 220;       // Forcer encoderValue à 350 si c'est le cas
+    MM.setAbsCounter(3, 220);  // Réinitialisez l'encodeur à 0
+  }
+  // Positionnement du servomoteur en fonction de la valeur mappée
+  myservoAnnulaire.write(mappedValue3);
+  // Affichage de la valeur de l'encodeur
+  Serial.print(encoderValue3);
+  delay(10);  // Délai entre chaque itération
+}
+
+void auriculaire() {
+  encoderValue4 = MM.getAbsCounter(4);  // Lecture de la position de l'encodeur
+  mappedValue4 = map(encoderValue4, 0, 100, 50, 100);
+
+  // Vérification des limites de l'encodeur
+  if (encoderValue4 <= 0) {
+    encoderValue4 = 0;
+    MM.setAbsCounter(4, 0);  // Réinitialisez l'encodeur à 0
+
+  } else if (encoderValue4 >= 220) {
+    encoderValue4 = 220;       // Forcer encoderValue à 350 si c'est le cas
+    MM.setAbsCounter(4, 220);  // Réinitialisez l'encodeur à 0
+  }
+  // Positionnement du servomoteur en fonction de la valeur mappée
+  myservoAuriculaire.write(mappedValue4);
+  // Affichage de la valeur de l'encodeur
+  Serial.println(encoderValue4);
+  delay(10);  // Délai entre chaque itération
+}
+
+
+ 
